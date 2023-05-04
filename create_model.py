d947bce506916e1fbc22eccda59a3bc834b95cf0
iputucaganesha <iputucaganesha@gmail.com>
2023-05-04 13:54:56 +0700

    Complete code
    Minimal working result for the data, changes in get_pexels_photos.py to rename the downloaded image with image_id and functional create_model.py code

--- create_model.py
+++ create_model.py
@@ -1,71 +1,62 @@
-import csv
 import os
-import urllib.request
 import cv2
 import numpy as np
-import tensorflow as tf
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 
-# Load CSV file
-csv_file = 'dataset.csv'
-data = []
-with open(csv_file, 'r') as f:
-    reader = csv.reader(f)
-    next(reader)  # skip header row
-    for row in reader:
-        image_id, image_url = row
-        data.append((image_id, image_url))
+# set up search criteria
+search_terms = ['lemon', 'lychee', 'grape', 'kiwi', 'apple'] # keywords for search
 
-# Download images and save to folder
-folder_name = 'images'
-if not os.path.exists(folder_name):
-    os.makedirs(folder_name)
+# set up parameters
+img_size = (128, 128) # size to resize images
+num_classes = len(search_terms) # number of fruit classes
 
-for image_id, image_url in data:
-    filename = os.path.join(folder_name, f'{image_id}.jpg')
-    urllib.request.urlretrieve(image_url, filename)
-
-# Load and preprocess images
+# load images and labels
 images = []
 labels = []
-for image_id, _ in data:
-    filename = os.path.join(folder_name, f'{image_id}.jpg')
-    img = cv2.imread(filename)
-    img = cv2.resize(img, (224, 224))
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = img.astype(np.float32) / 255.0  # normalize pixel values
-    images.append(img)
-    
-    # Get label from image ID
-    label = int(image_id.split('_')[-1])  # assume label is the last part of the image ID
-    labels.append(label)
+for i, term in enumerate(search_terms):
+    folder_name = f'{term}_images'
+    for filename in os.listdir(folder_name):
+        image_path = os.path.join(folder_name, filename)
+        try:
+            # read image and resize
+            img = cv2.imread(image_path)
+            img = cv2.resize(img, img_size)
+            images.append(img)
+            labels.append(i)
+        except Exception as e:
+            print(f'Error loading {image_path}: {e}')
 
-# Split data into training, validation, and testing sets
-x_train, x_test, y_train, y_test = train_test_split(images, labels, test_size=0.2, random_state=42)
-x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
+# convert to numpy arrays
+images = np.array(images)
+labels = np.array(labels)
 
-# Define CNN architecture
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
-    tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(5, activation='softmax')
-])
+# split into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.2, random_state=42)
 
-# Compile the model
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# convert labels to categorical
+y_train = to_categorical(y_train, num_classes)
+y_test = to_categorical(y_test, num_classes)
 
-# Train the model
-history = model.fit(x_train, y_train, epochs=10, validation_data=(x_val, y_val))
+# create model
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(img_size[0], img_size[1], 3)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(256, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
 
-# Evaluate the model on the testing set
-test_loss, test_acc = model.evaluate(x_test, y_test)
-print(f'Test accuracy: {test_acc:.2f}')
+# compile model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Save the model
-model.save('model.h5')
+# train model
+model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
--- get_pexels_photos.py
+++ get_pexels_photos.py
@@ -3,20 +3,22 @@ import csv
 import requests
 
 # set up search criteria
-search_terms = ['lemon', 'lychee', 'grape'] # keywords for search
+search_terms = ['lemon', 'lychee', 'grape', 'kiwi', 'apple'] # keywords for search
 
 # loop through each search term
 for term in search_terms:
-    # read CSV file to get image URLs
+    # read CSV file to get image URLs and image IDs
     csv_file = os.path.join(f'{term}_images', f'{term}_urls.csv')
     with open(csv_file, 'r') as f:
         reader = csv.reader(f)
         next(reader) # skip header row
         for row in reader:
+            image_id = row[0]
             image_url = row[1]
             response = requests.get(image_url)
             file_name = os.path.basename(image_url)
-            file_name = f'{term}_{file_name}'
+            file_ext = os.path.splitext(file_name)[1]
+            file_name = f'{image_id}{file_ext}'
             file_path = os.path.join(f'{term}_images', file_name)
             with open(file_path, 'wb') as f:
                 f.write(response.content)
